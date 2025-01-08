e7846e0a262bc9fe6a60be6782d362df5b9f0fcc
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-01-08 11:20:12 +0530

    Style homepage

--- app/page.js
+++ app/page.js
@@ -10,20 +10,27 @@ const ipDetails = [
 export default function Home() {
   return (
     <>
-      <header>
-        <Wrapper>
-          <h1>IP Address Tracker</h1>
-          <form>
+      <header className="min-h-[18.75rem] bg-[url('/images/backgrounds/pattern-bg-mobile.png')] bg-cover bg-center bg-no-repeat lg:min-h-[17.5rem] lg:bg-[url('/images/backgrounds/pattern-bg-desktop.png')]">
+        <Wrapper className="grid gap-7 pt-7">
+          <h1 className="text-center text-[1.625rem] font-medium text-white lg:text-[2rem]">
+            IP Address Tracker
+          </h1>
+          <form className="mx-auto flex w-full max-w-[34.6875rem]">
             <input
               type="search"
               placeholder="Search for any IP address or domain"
+              className="w-full cursor-pointer rounded-l-2xl bg-white px-6 placeholder:text-gray-400"
             />
-            <button type="submit">
+            <button
+              type="submit"
+              className="cursor-pointer rounded-r-2xl bg-black px-6 py-[1.375rem] transition-all hover:bg-gray-800"
+            >
               <img
                 src="/images/icons/icon-arrow.svg"
                 alt="Search"
                 width="11"
                 height="14"
+                className="min-w-fit"
               />
             </button>
           </form>
@@ -31,18 +38,25 @@ export default function Home() {
       </header>
 
       <main>
-        <section>
-          <Wrapper>
-            <dl>
+        <section className="relative">
+          <Wrapper className="absolute inset-x-0 -top-32 lg:-top-20">
+            <dl className="mx-auto grid max-w-[69.375rem] gap-6 rounded-2xl bg-white p-6 shadow-lg lg:grid-cols-4 lg:gap-0 lg:p-0">
               {ipDetails.map((detail) => (
-                <div key={detail.id}>
-                  <dt>{detail.name}</dt>
-                  <dd>{detail.value}</dd>
+                <div
+                  key={detail.id}
+                  className="grid gap-1.5 text-center lg:relative lg:content-start lg:gap-4 lg:px-8 lg:py-9 lg:text-left lg:not-first:before:absolute lg:not-first:before:inset-y-10.5 lg:not-first:before:left-0 lg:not-first:before:w-px lg:not-first:before:bg-gray-400 lg:not-first:before:content-['']"
+                >
+                  <dt className="text-[0.625rem] font-bold tracking-[0.16em] text-gray-400 uppercase lg:text-xs/[normal] lg:font-medium">
+                    {detail.name}
+                  </dt>
+                  <dd className="text-xl/[normal] font-medium lg:text-2xl/[normal] lg:tracking-[0.03em]">
+                    {detail.value}
+                  </dd>
                 </div>
               ))}
             </dl>
           </Wrapper>
-          <div id="map"></div>
+          <div id="map" className="min-h-96"></div>
         </section>
       </main>
     </>
