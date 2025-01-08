8c64539cdee8ce81b40bd118d076e6fa8eb42740
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-01-08 11:06:31 +0530

    Apply base styles to homepage body tag
    Set base colors and typography in the body tag to ensure consistent styling inheritance for descendants.

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,22 @@
-import { Rubik } from 'next/font/google';
-import './globals.css';
+import { Rubik } from "next/font/google";
+import "./globals.css";
 
 const rubik = Rubik({
-  subsets: ['latin'],
-  display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
-  title: 'Frontend Mentor | IP Address Tracker',
-  description: 'An IP address tracker app built with Next.js',
+  title: "Frontend Mentor | IP Address Tracker",
+  description: "An IP address tracker app built with Next.js",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={rubik.className}>
-      <body>{children}</body>
+      <body className="bg-white text-lg/[normal] text-gray-800">
+        {children}
+      </body>
     </html>
   );
 }
